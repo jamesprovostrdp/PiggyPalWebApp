325ef165b79dc4a82ed3d493ccba824abf034fa8
BuildTools <unconfigured@null.spigotmc.org>
2025-03-20 12:06:02 -0600

    Starting CSV File Parser Service

---
+++ PiggyPalWebApp/Services/CSVFileService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Data;
+
+namespace PiggyPalWebApp.Services
+{
+    public class CSVFileService
+    {
+
+        public CSVFileService()
+        {
+
+        }
+
+        public async Task<DataTable> ParseFileToDataTable(string filePath, string[] delimiters)
+        {
+            // Create and set up the parser and its delimiters
+            var parser = new TextFieldParser(filePath)
+            {
+                TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+            };
+            parser.SetDelimiters(delimiters);
+
+            string[] row = parser.ReadFields();
+
+            while (!parser.EndOfData)
+            {
+            }
+        }
+    }
+}
