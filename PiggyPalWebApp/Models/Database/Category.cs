d1c3ea24a2b8f4164325434834a220b5ce0d085a
james <jamesprovee@gmail.com>
2025-03-20 08:05:07 -0600

    Category, Goal, and Record Database Models created

---
+++ PiggyPalWebApp/Models/Database/Category.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+
+namespace PiggyPalWebApp.Models.Database
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Length(1, 25, ErrorMessage = "Name must be between 1 to 25 characters.")]
+        public string DisplayName { get; set; } = "Category";
+
+        [AllowNull]
+        [DataType(DataType.Currency)]
+        [Range(0, double.MaxValue, ErrorMessage = "Spending Limit must be a positive value.")]
+        public decimal? SpendingLimit { get; set; } = null;
+
+        [Required]
+        public bool IsExpense { get; set; } = true;
+
+        [Required]
+        public string BackgroundColor { get; set; } = Color.White.ToString();
+
+        public int UserID { get; set; }
+        public User? Owner { get; set; }
+
+        public ICollection<Record>? Records { get; set; }
+
+    }
+}
---
+++ PiggyPalWebApp/Models/Database/Goal.cs
@@ -0,0 +1,12 @@
+﻿namespace PiggyPalWebApp.Models.Database
+{
+    public class Goal
+    {
+        public int GoalId { get; set; }
+        public string GoalName { get; set; }
+
+        public double GoalAmount {  get; set; }
+
+        public DateTime DueDate {  get; set; }
+    }
+}
