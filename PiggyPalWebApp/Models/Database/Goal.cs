6e24a5cbd30ddbac4dcadbc505e1e406639eb55f
davidblackwell121 <david.blackwell121@gmail.com>
2025-03-25 13:27:19 -0600

    GoalService added to handle calculation logic for Goals

--- PiggyPalWebApp/Models/Database/Goal.cs
+++ PiggyPalWebApp/Models/Database/Goal.cs
@@ -26,39 +26,5 @@ namespace PiggyPalWebApp.Models.Database
         [Required]
         [DataType(DataType.Date, ErrorMessage = "Not a valid Date.")]
         public DateTime DueDate { get; set; } // Uses DateTime method to convert user input into a date
-
-        // Variable to track the amount required per paycheck to meet the goal
-        public double MonthlySavings => CalculateSavingsRequired();
-
-        // Method to calculate the amount required per paycheck to meet the goal before the due date
-        private double CalculateSavingsRequired()
-        {
-            // Check if the DueDate is in the past
-            if (DueDate < DateTime.UtcNow)
-            {
-                return 0; // Due date is already passed, cannot calculate savings per month
-            }
-
-            // Calculate the number of months left until the due date (present or future)
-            var monthsRemaining = (DueDate.Year - DateTime.UtcNow.Year) * 12 + DueDate.Month - DateTime.UtcNow.Month;
-
-            // Ensures monthsRemaining is at least 1 to prevent errors
-            if (monthsRemaining <= 0)
-            {
-                return 0; // No time left, cannot calculate the savings per month
-            }
-
-            // Calculate the total amount left to save
-            double amountToSave = SavingsGoal - CurrentSavings;
-
-            // If no savings are needed, return 0
-            if (amountToSave <= 0)
-            {
-                return 0;
-            }
-
-            // Calculate the required savings per month
-            return amountToSave / monthsRemaining;
-        }
     }
 }
