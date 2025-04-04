a40c324a63bcf04e8741dd779cca2d33d9227ea2
JaimiMcDiarmid <jaimilynn101@gmail.com>
2025-04-04 15:39:52 -0600

    Working on getting charts ready

---
+++ PiggyPalWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PiggyPalWebApp.Models;
+
+namespace PiggyPalWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateIncomeCategory(MainViewModel viewModel)
+        {
+            viewModel.Categories.Add(viewModel.PlaceholderCategory);
+
+            return RedirectToAction("Main", "Home", viewModel);
+        }
+    }
+}
